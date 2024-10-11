cdc8fc62db917f8bba2e0aecbc1474ab9486f63c
babusarath05 <babusarath05@gmail.com>
2024-10-11 05:37:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ import streamlit as st
 # import datetime
 import google.generativeai as genai
 
+cve_report = '{"vendor":{"0":"MITRE","1":"MITRE","2":"MITRE","3":"MITRE","4":"MITRE","5":"MITRE","6":"Solidigm","7":"Solidigm","8":"Solidigm","9":"Solidigm","10":"Solidigm","11":"Solidigm","12":"Solidigm","13":"Solidigm","14":"Solidigm","15":"JetBrains s.r.o.","16":"JetBrains s.r.o.","17":"JetBrains s.r.o.","18":"JetBrains s.r.o.","19":"MITRE","20":"MITRE","21":"MITRE","22":"MITRE","23":"MITRE","24":"MITRE"},"epss":{"0":"Not Available","1":"Not Available","2":"Not Available","3":"Not Available","4":"Not Available","5":"Not Available","6":"0.04%","7":"0.04%","8":"0.04%","9":"0.04%","10":"0.04%","11":"0.04%","12":"0.04%","13":"0.04%","14":"0.04%","15":"0.04%","16":"0.04%","17":"0.04%","18":"0.04%","19":"0.04%","20":"0.04%","21":"0.05%","22":"0.04%","23":"0.04%","24":"0.05%"},"cvss":{"0":"Not Available","1":"Not Available","2":"Not Available","3":"Not Available","4":"Not Available","5":"Not Available","6":"6.7","7":"7.0","8":"4.4","9":"5.1","10":"4.0","11":"6.5","12":"6.2","13":"4.4","14":"10.0","15":"3.5","16":"3.5","17":"4.9","18":"4.9","19":"Not Available","20":"6.7","21":"7.2","22":"Not Available","23":"6.1","24":"7.5"},"publish_date":{"0":"2024-10-10","1":"2024-10-10","2":"2024-10-10","3":"2024-10-09","4":"2024-10-09","5":"2024-10-09","6":"2024-10-07","7":"2024-10-07","8":"2024-10-07","9":"2024-10-07","10":"2024-10-07","11":"2024-10-07","12":"2024-10-07","13":"2024-10-07","14":"2024-10-07","15":"2024-10-08","16":"2024-10-08","17":"2024-10-08","18":"2024-10-08","19":"2024-10-04","20":"2024-10-04","21":"2024-10-04","22":"2024-10-04","23":"2024-10-04","24":"2024-10-04"}}'
 
 def generate_response(gemini_api_key,prompt):
     txt = "You are called ARI: Audit Report Interpreter AI. You help to interpret Cybersecurity Audit Dcouments and provide appropriate results\n"
@@ -140,7 +141,7 @@ if prompt := st.chat_input("You can add your questions here"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-        print("<<<"+prompt)
+        # print("<<<"+prompt)
     
     
     # Display assistant response in chat message container
@@ -152,6 +153,10 @@ if prompt := st.chat_input("You can add your questions here"):
                 if audit_file is not None:
                     context = RAG(audit_file.name,prompt)
                     prompt = "Information:\n"+context+"Question:\n"+prompt
+                    print("<<<"+prompt)
+                else:
+                    cve_report = load_cve_data()
+                    prompt+="\n\nThe following is the Security vulnerabilities CVE report published in 2024:\n"+cve_report+"\n"
                     print("<<<"+prompt)
                     
                 assistant_response = generate_response(gemini_api_key,prompt)
